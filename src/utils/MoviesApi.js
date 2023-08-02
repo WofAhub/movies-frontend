1a1bdc15457d03f990c9f6381ed1fa6a4a45ac59
WofA <wofamyworld@gmail.com>
2023-08-03 01:13:45 +0300

    Исправил импорт у MainApi и MoviesApi

--- src/utils/MoviesApi.js
+++ src/utils/MoviesApi.js
@@ -1,5 +1,5 @@
 import { MOVIES_URL } from "./constants.js";
-import { checkAnswerFromServer } from './function/function.js';
+import checkAnswerFromServer from './function/function.js';
 
 // получить фильмы
 export function getMovies() {
